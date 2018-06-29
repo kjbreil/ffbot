1642f6d9f73bcf1d5d642519fe8e1dbd265abe0d
Kjell Breiland <kjell@kaygel.io>
2018-06-28 22:58:39 -0700

    get it going

---
+++ fantasy/util.go
@@ -0,0 +1,10 @@
+package fantasy
+
+import (
+	"fmt"
+)
+
+// Holder is just to facilitate creating project structure
+func Holder(s string) string {
+	return fmt.Sprintf("Why would you type %s", s)
+}
---
+++ ffbot.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/kjbreil/ffbot/fantasy"
+)
+
+func main() {
+	// for now read from console and send input to function printing output
+
+	// open a reader from stdin
+	r := bufio.NewReader(os.Stdin)
+
+	// forever for loop
+	for {
+		// use fmt instead of log
+		fmt.Print("::")
+		// assign texxt when a newline is detected
+		text, err := r.ReadString('\n')
+		// drop the newline from text
+		text = strings.Replace(text, "\n", "", -1)
+
+		if err != nil {
+			// use log since its a failure and timestamp is desired
+			log.Fatal(err)
+		}
+		// hold me
+		fantasy.Holder(text)
+	}
+}
